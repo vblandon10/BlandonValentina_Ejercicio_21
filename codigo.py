ad941f555fafbd6d94302d0c193e2a398f2b93a9
Default user <v.blandon10@uniandes.edu.co>
2019-10-22 18:33:47 +0000

    grafica seno

--- codigo.py
+++ codigo.py
@@ -5,4 +5,7 @@ x= np.linspace(-2*np.pi, 2*np.pi, 100)
 funcion =np.sin(x)
 
 plt.plot(x,funcion)
-plt.show()
+plt.ylabel("y")
+plt.xlabel("x")
+plt.title("Grafica seno")
+plt.savefig("graficasen.png")
